b9266eab77700f656634f27ac7ace1626cb127b5
behibobo <behzadfareed@gmail.com>
2020-09-23 22:36:46 +0330

    sss

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_secure_password
 
     enum role: [:user, :admin]
+
+    def self.starts_with(column_name, prefix)
+        where("lower(#{column_name}) like ?", "#{prefix.downcase}%")
+    end
 end
