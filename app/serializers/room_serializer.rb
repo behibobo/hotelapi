ff5a9dd2590bace1890ff74c185fd7ffbd5d4e14
behzad <behzadfareed@gmail.com>
2020-10-08 14:48:25 +0330

    sss

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -1,10 +1,18 @@
 class RoomSerializer < ActiveModel::Serializer
-  attributes :id, :number, :check_in_hour, :check_out_hour, :images
+  attributes :id, :number, :check_in_hour, :check_out_hour, :images, :facilities, :room_facilities
   has_one :hotel
   has_one :room_type
   
   
   def images
     object.images.map { |image| {id: image.id, url: image.name.url} }
+  end
+
+  def facilities
+    object.facilities.pluck(:id)
+  end
+
+  def room_facilities
+    object.facilities.map {|f| {id: f.id, name: f.name }}
   end
 end
