e092aa61fa79bf471273bcd8198d08c4ae8925c0
behzad <behzadfareed@gmail.com>
2020-10-08 16:14:19 +0330

    sss

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,5 +1,13 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :check_in_date, :check_out_date, :book_date, :status
+  attributes :id, :check_in_date, :check_out_date, :book_date, :status, :room_type
   has_one :room
   has_one :passenger
+
+  def room_type
+    object.room.room_type
+  end
+
+  def status
+    Booking.statuses[object.status]  
+  end
 end
